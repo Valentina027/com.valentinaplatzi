8c777e91bad792b94fc8ff6f246a2a351125805f
VALENTINA M <valentina.m2427@gmail.com>
2022-07-07 11:43:33 -0500

    TEST JAVA

---
+++ src/main/java/com/valentinaplatzi/javatests/util/StringUtil.java
@@ -0,0 +1,8 @@
+package com.valentinaplatzi.javatests.util;
+
+public class StringUtil {
+
+    public String repeat(String str, int times){
+        return null;
+    }
+}
---
+++ src/test/java/com/valentinaplatzi/javatests/util/StringUtilTest.java
@@ -0,0 +1,8 @@
+package com.valentinaplatzi.javatests.util;
+
+public class StringUtilTest {
+
+    public static void main(String[] args) {
+        
+    }
+}
